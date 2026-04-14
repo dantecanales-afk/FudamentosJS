47d9f1a209ba384c2ec6210b12a4173ab1839ec5
dantesillo <dantecanales@liceovvh.cl>
2026-04-14 10:02:57 -0400

    ok

---
+++ 01_introduccion/Hola_mundo/static/js/script.js
@@ -0,0 +1,12 @@
+console.log("hola mundo, desde archivo externo...");
+
+console.log("conexion existosa con el js externo...");
+
+const saludo = "hola a todos, ";
+let destinatario = "estudiante";
+console.log(saludo +"para los " + destinatario);
+
+// 2) saludar a una persona ingresada por teclado
+let nombre = prompt ("ingrese su nombre: ");
+
+console.log("hola " + nombre)
---
+++ 02_tipos_de_datos/1_tipo_texto/static/js/script.js
@@ -0,0 +1,9 @@
+console.log("conexion con js correcta...")
+/*
+concatenacion con el signo + 
+podemos uir texto y variables
+*/
+const nombre = "Dante";
+const apellido = "Canales";
+//unimos ambos const con un texto extra
+console.log("hola mi nombre es: " + nombre + " " + apellido );
