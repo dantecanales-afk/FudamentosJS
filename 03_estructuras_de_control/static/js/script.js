4dc65cb7a7acbd9ae9234f5aed2cb89ea3e86998
dantesillo <dantecanales@liceovvh.cl>
2026-04-21 14:13:30 -0400

    ok

---
+++ 03_estructuras_de_control/static/js/script.js
@@ -0,0 +1,19 @@
+console.log(`conexion con js`)
+//tipos de datos numericos//
+
+//1- declaracion de numeros(creacion de variables numericoas)//
+let edad ="31"
+let temperatura ="-5"
+let precio= "1,999"
+let pi ="3.1415"
+let descuento ="0.25"
+
+
+//operaciones basicas
+
+let suma =10 + 5; //15
+let resta =10 - 5 //5
+let multiplicacion = 10 * 5; //50
+let division = 10 / 2; //5
+let modulo = 10 % 3; //1
+let potencia = 2 ** 3 //1
